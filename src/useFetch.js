da38a35d8e8196468987415907d6412c53c95580
Wajdan-Faheem <wajdanf02@gmail.com>
2021-06-10 22:45:34 -0400

    Routesss

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,9 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const  abort = new AbortController();
     setTimeout(() => {
-      fetch(url)
+      fetch(url, {signal: abort.signal})
       .then(res => {
         if (!res.ok) { // error coming back from server
           throw Error('could not fetch the data for that resource');
@@ -20,11 +21,18 @@ const useFetch = (url) => {
         setError(null);
       })
       .catch(err => {
+        if (err.name === "AbortError"){
+          console.log("fetch aborted");
+        }
+        else{
         // auto catches network / connection error
         setIsPending(false);
         setError(err.message);
+        }
       })
     }, 1000);
+
+    return ()=>abort.abort();//abort throws an error
   }, [url])
 
   return { data, isPending, error };
