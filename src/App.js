40c99e943dde1d766bff049827b091c419d6121f
Wajdan-Faheem <wajdanf02@gmail.com>
2021-06-12 03:39:22 -0400

    stuff

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
 
                       <Route path="/blogs/:id">
                           <BlogDetails />
-                          <Create />
+                          
                       </Route>
                   </Switch>
               </div>
--- src/blogdetails.js
+++ src/blogdetails.js
@@ -1,12 +1,22 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch"
 
 const BlogDetails = () => {
 
     const {id }= useParams();
-
+    const { data, error, isPending } = useFetch("http://localhost:8000/blogs/"+id);
+ 
     return (   
         <div className="blog-BlogDetails">
-            <h2>Blog Details - {id}</h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {data && (
+                <article>
+                    <h2>{data.title}</h2>
+                    <div>Written by: {data.author}</div>
+                    <div>{data.body} </div>
+                </article>
+            )}
         </div>
 
      );
